1ce836f899f079733fd64bf285d1b23f9e31b1f8
RachelChen <iceage91@gmail.com>
2022-03-20 20:54:42 +0800

    Get articles data from API to display  corresponding page

---
+++ src/pages/Article.jsx
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import moment from "moment"
+
+const API_URL = "https://hex-escape-room.herokuapp.com"
+const API_PATH_ARTICLES = "/api/react/articles"
+
+function Article() {
+  const [articles, setArticles] = useState([])
+
+  const fetchData = async () =>{
+    try{
+    const {data, statusText} = await axios.get(API_URL + API_PATH_ARTICLES)
+    if(statusText === "OK"){
+      setArticles([...data.articles])
+    }
+    }catch(err){
+      console.log(err)
+    }
+
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div className="container">
+      文章
+      <div className="card">
+        <div className="card-header bg-white">
+          <h5 className="">文章列表</h5>
+        </div>
+        <div className="table-responsive">
+          <table className="table table-hover table-nowrap">
+            <thead className="table-light">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">文章名稱</th>
+                <th scope="col">文章描述</th>
+                <th scope="col">文章作者</th>
+                <th scope="col">文章日期</th>
+                <th scope="col">文章狀態</th>
+                <th scope="col">文章標籤</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {
+                articles.map((article, index) => {
+                  return <tr key={article.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{article.title}</td>
+                    <td>{article.description}</td>
+                    <td>{article.author}</td>
+                    <td>{moment(article.create_at).format('YYYY-MM/DD HH:mm:ss')}</td>
+                    <td>{article.isPublic ? '公開' : '未公開'}</td>
+                    <td>{article.tag.map(tag => tag)}</td>
+                    <td>
+                      <Link to={article.id} className="btn btn-sm btn-neutral">查看</Link>
+                    </td>
+                  </tr>
+                })
+              }
+
+            </tbody>
+          </table>
+        </div>
+        <div className="card-footer bg-white py-3 text-end">
+          <span className="text-muted text-sm ">總共{articles.length}筆資料</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Article;
--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -1,4 +1,64 @@
-<div className="container">
+import axios from "axios"
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+const API_URL = "https://hex-escape-room.herokuapp.com"
+const API_PATH_ARTICLES = "/api/react/article/"
+
+function ArticleDetail () {
+  const [articleDetail, setArticleDetail] = useState({title:"", author:"", content: "", tag: []})
+  const { articleId } = useParams()
+
+  const fetchData = async () => {
+    try{
+      const { data, statusText } = await axios.get(API_URL + API_PATH_ARTICLES + articleId)
+      if(statusText === "OK"){
+        setArticleDetail(data.article)
+      }
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  const onArticleChange = (e) => {
+    const { id, value } = e.target
+    setArticleDetail({
+      ...articleDetail,
+      [id]: value
+    })
+  }
+
+  const onTagChange = (e) => {
+    const { id, value } = e.target
+    if(!value.trim().length) return
+    const temp = [...articleDetail.tag]
+    temp[Number(id)] = value
+
+    if(id === 'tag') {
+       temp.push(value)
+       e.target.blur()
+    }
+    setArticleDetail({
+       ...articleDetail,
+       tag: [...temp]})
+  }
+  
+  const handleRemoveTag = (e) => {
+    const { id } = e.target
+    const temp = [...articleDetail.tag]
+    Number(id) > 0 && temp.splice(Number(id), 1)
+    Number(id) === 0 && temp.shift()
+    setArticleDetail({
+       ...articleDetail,
+       tag: [...temp]})
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div className="container">
   <div className="row">
     <div className="col-md-6">
       <div className="card">
@@ -9,20 +69,26 @@
           <form>
             <div className="mb-3">
               <label htmlFor="title" className="form-label">文章名稱</label>
-              <input type="text" className="form-control" id="title" />
+              <input type="text" className="form-control" id="title" value={articleDetail.title} onChange={onArticleChange}/>
             </div>
             <div className="mb-3">
               <label htmlFor="author" className="form-label">文章作者</label>
-              <input type="text" className="form-control" id="author" />
+              <input type="text" className="form-control" id="author" value={articleDetail.author} onChange={onArticleChange}/>
             </div>
             <div className="mb-3">
               <label htmlFor="content" className="form-label">文章描述</label>
-              <textarea className="form-control" id="content" />
+              <textarea className="form-control" id="content" value={ articleDetail.content } onChange={onArticleChange}/>
             </div>
             <div className="row gx-1 mb-3">
+               { articleDetail.tag.length > 0 && articleDetail.tag.map((tag, index) => {
+                    return  <div className="col-md-2 mb-1" key={index}><div className="input-group input-group-sm">
+                  <input type="text" className="form-control form-control" id={`${index}`} placeholder="請輸入標籤" value={tag} onChange={onTagChange}/>
+                  <button type="button" className="btn btn-outline-danger" id={`${index}`} onClick={handleRemoveTag} >x</button>
+                </div></div>
+                  }) }
               <div className="col-md-2 mb-1">
                 <div className="input-group input-group-sm">
-                  <input type="text" className="form-control form-control" id="tag" placeholder="請輸入標籤" />
+                  <input type="text" className="form-control form-control" id="tag" placeholder="請輸入標籤" value="" onChange={onTagChange} />
                   <button type="button" className="btn btn-outline-danger">x</button>
                 </div>
               </div>
@@ -37,11 +103,13 @@
           <h5 className="card-title">文章圖片</h5>
         </div>
         <div className="card-body">
-          ...
+          <img src={articleDetail.imgage} alt="" />
         </div>
       </div>
     </div>
   </div>
-
-
 </div>
+  )
+}
+
+export default ArticleDetail
